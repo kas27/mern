3b7b9d083eb1407b74cdf0b96279ab1b7afac65e
yaroslav <yaroslav@vidax.com>
2020-02-18 12:14:38 +0200

    update

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const app = express();
 const PORT = config.get('port') || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../client', 'build')));
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname), '../client', 'build', 'index.html');
+    res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html');
   });
 }
 
